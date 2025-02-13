9d93f1764aafa45cc8ec320c973131134d36e450
magomed <mrnavruzov@edu.hse.ru>
2025-02-14 01:10:28 +0300

    Changed main.cpp to work with a new class

--- main.cpp
+++ main.cpp
@@ -1,78 +1,24 @@
 #include <sqlite3.h>
 
 #include <cstring>
+#include <memory>
 
 #include "crow.h"
-#include "placesDB.h"
-#include "validation.h"
+#include "placesApp.h"
 
 int main() {
-	std::string pathToDB = "storage.db";
-	PlacesDB* db;
-	try {
-		db = new PlacesDB(pathToDB);
-	} catch (const std::exception& e) {
-		CROW_LOG_ERROR << "Error: " << e.what();
-		return 1;
-	}
-	try {
-		db->create();
-	} catch (const std::exception& e) {
-		CROW_LOG_ERROR << "Error: " << e.what();
-		return 1;
-	}
+	const std::string dbPath = "storage.db";
+	PlacesApp placesApp(dbPath);
 
 	crow::SimpleApp app;
 	CROW_ROUTE(app, "/places")
-		.methods(crow::HTTPMethod::POST)([&db](const crow::request& req) {
-			auto reqJson = crow::json::load(req.body);
-			if (!validate(reqJson))
-				return crow::response(crow::status::BAD_REQUEST);
-			std::string name = reqJson["name"].s();
-			std::string type = reqJson["type"].s();
-			auto coordinates = reqJson["coordinates"].lo();
-			double latitude = coordinates[0].d();
-			double longitude = coordinates[1].d();
-
-			try {
-				db->insert({name, type, latitude, longitude});
-			} catch (const std::exception& e) {
-				CROW_LOG_ERROR << "Error: " << e.what();
-				return crow::response(crow::status::INTERNAL_SERVER_ERROR);
-			}
-
-			crow::response res;
-			res.code = crow::status::OK;
-			res.body = "Place added!";
-			return res;
-		});
+		.methods(crow::HTTPMethod::POST)(
+			[&placesApp](const crow::request& req) {
+				return placesApp.addPlace(req);
+			});
 	CROW_ROUTE(app, "/places")
-		.methods(crow::HTTPMethod::GET)([&db](const crow::request& req) {
-			std::string type;
-			if (req.url_params.get("type")) type = req.url_params.get("type");
-
-			std::vector<Place> places;
-			try {
-				places = db->get(type);
-			} catch (const std::exception& e) {
-				CROW_LOG_ERROR << "Error: " << e.what();
-				return crow::response(crow::status::INTERNAL_SERVER_ERROR);
-			}
-			crow::json::wvalue respJson;
-			int i = 0;
-			for (const auto& place : places) {
-				respJson[i] = {
-					{"name", place.name},
-					{"type", place.type},
-					{"coordinates", crow::json::wvalue::list(
-										{place.latitude, place.longitude})}};
-				i++;
-			}
-
-			crow::response res;
-			res.code = crow::status::OK;
-			res.body = respJson.dump();
-			return res;
+		.methods(crow::HTTPMethod::GET)([&placesApp](const crow::request& req) {
+			return placesApp.getPlaces(req);
 		});
 
 	app.bindaddr("127.0.0.1").port(8080).multithreaded().run();
